67de6316a6f563da16fd10590d8f50fc4c95ff7f
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-08-05 10:59:23 +0530

    style - search bar same width as cards

--- src/Components/LandingPage/SearchBar-sc.ts
+++ src/Components/LandingPage/SearchBar-sc.ts
@@ -3,11 +3,11 @@ import { COLORS } from "../../Constants/Colors";
 
 export const SearchBar = styled.input`
     padding: 0.5rem;
-    width: 100%;
+    margin: 0 0 0.5rem 0.5rem;
+    width: calc(100% - 1.5rem);
     border: 1px solid ${COLORS.border};
     box-shadow: 0 1px 1px ${COLORS.textGray};
     border-radius: 0.25rem;
-    margin-bottom: 1rem;
     outline: none;
 
     &:focus {
